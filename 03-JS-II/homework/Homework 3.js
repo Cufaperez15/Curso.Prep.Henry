edca258770a8a6f27cb70905f6050b1499d8ab7f
Facundo Pérez <you@example.com>
2021-07-28 20:20:02 -0300

    homework 3 terminado

---
+++ 03-JS-II/homework/Homework 3.js
@@ -0,0 +1,12 @@
+Homework 
+* `for` // Es un bucle. Es unica, similar a IF. pero mas compleja. 
+// Primero irá la palabra for, seguida del paréntesis y luego abro y cierro llaves. 
+// El bucle for evaluará la expresión condicional.
+* `&&` // Significa 'Y' ('and'). Se escriben los dos simnbolos. 
+//Evalua ambas expresiones y devuelve verdadero si ambas expresiones son TRUE. 
+//Si una de ambas es falso, devuelve FALSE
+ `||` // Significa 'O' ('or'). Se escribe ambas barras laterales. 
+ // Determinará si una de las expresiones es TRUE. Devuelve TRUE si una o ambas expresiones son TRUE
+ // Devuelve FALSE si ambas expresiones son FALSE.
+ `!` // Signica 'NOT'. Es un signo de exclamación. Vemos este operador antes de determinar la igualdad.
+ // El operador not devolverá el valor booleano opuesto de lo que pasa. 
